70a2bd745a66ff7b9e4b487a605fcf6a24d45eb3
pharr117 <pjharral@gmail.com>
2022-04-02 17:50:28 -0400

    Initial commit, uses CoinGecko and CoinHall (as a fallback API option) to get cost data for Accointing output symbol/date combinations

---
+++ README.md
@@ -0,0 +1,114 @@
+# Cost-Calc Script
+
+Run this script on an Accointing Excel export file and it will reach out to [CoinGecko](https://www.coingecko.com/) to get the costs of symbols according to the following metrics:
+
+* Specific classifications such as `staked` or `airdrops`
+* Based on the date column for specific years
+
+It will then output a new file with all the original rows except for a few differences:
+
+* A new column `usdValue`
+* For every row that matched the metric, this column will be filled out with the following formula: `<Symbol cost on that date in USD> * <number of coins of that symbol> = usdValue`
+
+As a fallback option, if CoinGecko does not provide full time-range coverage for all Symbol + Date combinations, the script will reach out to Coinhall (a Terra price tracker). This will only work for missing Terra symbols.
+
+# Requirements
+
+* Python 3+
+* Python packages: The script requires some imports
+    * `requests` for reaching out to the CoinGecko API
+    * `openpyxl` for reading and writing Excel files.
+    * You can install these into your Python environment by running `python -m pip install -r requirements.txt`
+* A preconfigured CoinGecko Coins config JSON file
+    * Containing the following:
+        * A dictionary with keys of Symbols (as found in your Accointing export)
+        * The values of these keys must contain CoinGecko information relating to the CoinGecko API request for that symbol
+    * A preconfigured list already comes with the package, make sure to check that out to see the default configuration and make sure it matches up to your use-case
+    * The values must contain the ID used by CoinGecko for the corresponding symbol
+    * E.g.
+
+```
+{
+    "DAI": {
+        "id": "dai",
+        "symbol": "dai",
+        "name": "Dai"
+    },
+    "ADA": {
+        "id": "cardano",
+        "symbol": "ada",
+        "name": "Cardano"
+    },
+    "ANC": {
+        "id": "anchor-protocol",
+        "symbol": "anc",
+        "name": "Anchor Protocol"
+    },
+    ...
+}
+```
+
+* You can check and/or add new values to this config file by running the following command and analyzing the output `coingecko_coins.json` file. You would then add new Symbol/config pairs for any Symbol you want
+
+```
+python -c "import json; import requests; print(json.dumps(requests.get('https://api.coingecko.com/api/v3/coins/list').json(), indent=4))" > coingecko_coins.json
+```
+
+* A CoinHall symbol config json file is required as well
+    * If using certain Terra coins, CoinGecko may not cover them. The code falls back on Coinhall if CoinGecko did not provide coverage
+    * Containing the following:
+        * A dictionary with keys of Symbols (as found in your Accointing export)
+        * The values of these keys must contain Coinhall information relating to the Coinhall API request for that symbol
+        * Coinhall tracks the symbol by Terra pair address, see the Coinhall implementation for details
+```
+#e.g.
+{
+    "MINE": {
+        "id": "terra178jydtjvj4gw8earkgnqc80c3hrmqj4kw2welz", #Symbol pair address
+        "symbol": "mine",
+        "name": "Pylon Protocol"
+    },
+    "PSI": {
+        "id": "terra163pkeeuwxzr0yhndf8xd2jprm9hrtk59xf7nqf",
+        "symbol": "psi",
+        "name": "Nexus Protocol"
+    },
+    "LOOP": {
+        "id": "terra106a00unep7pvwvcck4wylt4fffjhgkf9a0u6eu",
+        "symbol": "loop",
+        "name": "LOOP Finance"
+    },
+    ...
+}
+```
+
+# How to run
+
+The script takes a few configuration options, some with defaults, some without. You can see all the options by running `python cost_calc.py -h`
+
+The required options are as follows:
+
+* `-i` The input Excel file path to process
+* `-o` The output Excel file path to create
+* `-stoif` The configuration JSON file path
+
+So, to run you would do something like this:
+
+`python cost_calc.py -i path/to/input.xlsx -o path/to/output.xlsx -cgstoid path/to/coingecko/config.json -chstoid path/to/coinhall/config.json`
+
+You will then need to examine the output file to confirm the process worked after it has finished.
+
+# Gotchas
+
+The script is 100% reliant on the CoinGecko API. This comes with 2 gotchas:
+
+1. CoinGecko stores data on their servers for every coin. **If CoinGecko does not have data for a specific symbol on a specific date, this script will NOT be able to get the cost.** When this happens, the script will output the Excel lines so you can fill in the gaps where needed.
+2. If you forget to put a symbol config value in your `config.json` file, the script will **NOT** be able to fill in the cost for that symbol. Symbol names are not unique, so these cannot be reliad on to find the correct cost from CoinGecko.
+3. CoinGecko has a rate-limit and there is no current way to provide and API key if you pay for a higher rate-limit. You will have to wait at least a few minutes (depending on how many transactions are being processed)
+
+The script has a fallback API request structure that will reach out to Coinhall for symbols that do not have coverage in CoinGecko. This comes 2 with gotchas:
+
+1. Coinhall is Terra specific, it will not provide coverage outside of Terra coins
+2. Coinhall tracks price in quotes by uusd and pair address - Terra provides pair addresses that Coinhall uses to track Open/High/Low/Close values.
+
+
---
+++ staking-rewards.py
@@ -0,0 +1,373 @@
+import requests
+import time
+import sys
+import json
+import argparse
+from openpyxl import load_workbook
+from openpyxl.utils.exceptions import InvalidFileException
+from os.path import exists
+from openpyxl import Workbook
+import datetime
+import traceback
+
+COIN_GECKO_API_URL = "https://api.coingecko.com/api/v3"
+COIN_GECKO_COINS_ENDPOINT = "/coins"
+COIN_GECKO_HISTORICAL_ENDPOINT = "/<coin-id>/history"
+
+#this represents CoinGecko's 50 requests/minute API rate limit, plus some seconds for variance
+COIN_GECKO_REQUEST_CHUNKS = 50
+COIN_GECKO_THROTTLE_TIME = 70
+
+#this represents throttling for Coinhall, which will throttle after every single request
+COINHALL_THROTTLE_TIME = 10
+COINHALL_API_URL = "https://api.coinhall.org/api/v1"
+COINHALL_CHART_ENDPOINT = "/charts/terra/candles"
+COINHALL_HISTORICAL_PARAMS = "?bars=1&from=<from-time>&to=<to-time>&quoteAsset=uusd&interval=1d&pairAddress=<coin-id>"
+
+KEY_CLASSIFICATIONS = ["staked", "airdrop"]
+
+class CaughtError(Exception):
+    pass
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Get the cost-basis in USD (from CoinGecko) for your staking and airdrop rewards from Accointing export data')
+    parser.add_argument('--input-file', '-i', help='The location of your Accointing input file', required=True)
+    parser.add_argument('--output-file', '-o', help='The location of your output file', required=True)
+    parser.add_argument('--coingecko-symbol-to-id-file', '-cgstoid', help='The location of your CoinGecko JSON config file that stores symbols to ID configurations for making API requests', required=True)
+    parser.add_argument('--coinhall-symbol-to-id-file', '-chstoid', help='The location of your CoinHall JSON config file that stores symbols to ID configurations for making API requests', required=True)
+    parser.add_argument('--symbol-column', '-sc', help='The column in the EXCEL that stores the symbol of interest', default="boughtCurrency")
+    parser.add_argument('--value-column', '-vc', help='The column in the EXCEL that stores the symbol of interest', default="boughtQuantity")
+    parser.add_argument('--date-column', '-dc', help='The column in the EXCEL that stores the symbol of interest', default="timeExecuted")
+    parser.add_argument('--year-filter', '-yf', help='The year to gather data for', default=2021, type=int)
+    args = parser.parse_args()
+    return args
+
+def validate_args(args):
+    if not exists(args.input_file):
+        raise CaughtError(f"No file exists at {args.input_file} for the input data file, please enter a correct filepath")
+
+    if exists(args.output_file):
+        answer = input("Output file already exists, would you like to overwrite it? (Y/n) >> ")
+        if answer != "Y":
+            raise CaughtError("Exiting...")
+    
+    if not exists(args.coingecko_symbol_to_id_file):
+        raise CaughtError(f"No file exists at {args.coingecko_symbol_to_id_file} for the CoinGecko symbol to id configuration file, please enter a correct filepath")
+    
+    if not exists(args.coinhall_symbol_to_id_file):
+        raise CaughtError(f"No file exists at {args.coinhall_symbol_to_id_file} for the Coinhall symbol to id configuration file, please enter a correct filepath")
+
+def load_config_file(fname):
+    return json.load(open(fname))
+
+def parse_input_data(fname):
+    print("Loading data from Excel file")
+    wb = None
+    try:
+        wb = load_workbook(filename=fname)
+    except InvalidFileException:
+        raise CaughtError(f"Input file '{fname}' is not in the correct Excel format, please check your file and try again")
+
+    ws = wb.active
+    rows = ws.rows
+    headers = [c.value for c in next(rows)]
+    active_sheet_name = ws.title
+    return active_sheet_name, headers, [row for row in iter_worksheet(ws)]
+
+def iter_worksheet(worksheet):
+    # It's necessary to get a reference to the generator, as 
+    # `worksheet.rows` returns a new iterator on each access.
+    rows = worksheet.rows
+
+    # Get the header values as keys and move the iterator to the next item
+    keys = [c.value for c in next(rows)]
+    for row in rows:
+        values = [c.value for c in row]
+        yield dict(zip(keys, values))
+
+def get_key_data_point_indexes(rows, dateColumn, yearFilter):
+    print("Gathering key data points for classifications", KEY_CLASSIFICATIONS)
+    return [index for index, row in enumerate(rows) if "classification" in row and row["classification"] in KEY_CLASSIFICATIONS and row[dateColumn].year == yearFilter]
+
+def process_rows(rows, key_data_point_indexes, coingecko_symbols_to_id_configs, coinhall_symbols_to_id_configs, date_column, symbolColumn, valueColumn):
+
+    symbols_to_dates_to_rows = {}
+
+    #build data structure storing symbols to dates to row indexes
+    #used for ease-of-access and processing
+    for i in key_data_point_indexes:
+        row = rows[i]
+        boughtCurrency = row[symbolColumn]
+        if boughtCurrency not in symbols_to_dates_to_rows:
+            symbols_to_dates_to_rows[boughtCurrency] = {}
+
+        date_key = row[date_column].strftime("%d-%m-%Y")
+
+        if date_key not in symbols_to_dates_to_rows[boughtCurrency]:
+            symbols_to_dates_to_rows[boughtCurrency][date_key] = [i]
+        else:
+            symbols_to_dates_to_rows[boughtCurrency][date_key].append(i)
+
+
+    #loop through chunks of data and send to API for processing
+    print("Reaching out to CoinGecko for symbol cost data, this may take a while...")
+    symbols_to_dates_to_costs = {}
+    for chunk in chunk_symbols_and_dates(symbols_to_dates_to_rows, COIN_GECKO_REQUEST_CHUNKS):
+        for symbol in chunk:
+
+            request_url = get_coingecko_request_url(symbol, coingecko_symbols_to_id_configs)
+            
+            if symbol not in symbols_to_dates_to_costs:
+                symbols_to_dates_to_costs[symbol] = {}
+            
+            for date in chunk[symbol]:
+                symbols_to_dates_to_costs[symbol][date] = None
+
+                if request_url:
+                    parameterized_url = add_coingecko_request_params(request_url, date)
+
+                    resp = make_coingecko_api_request(parameterized_url, "Error reaching out to CoinGecko, please try again later:", COIN_GECKO_THROTTLE_TIME)
+
+                    symbols_to_dates_to_costs[symbol][date] = resp.json()
+                else:
+                    print("Your CoinGecko symbol config list does not support symbol", symbol)
+                
+        time.sleep(COIN_GECKO_THROTTLE_TIME)
+    
+    print("Finished reaching out to CoinGecko")
+
+    missing_coingecko_coverage = {}
+
+    for i in key_data_point_indexes:
+        row = rows[i]
+
+        boughtCurrency = row[symbolColumn]
+        date_key = row[date_column].strftime("%d-%m-%Y")
+
+        try:
+            row["usdValue"] = symbols_to_dates_to_costs[boughtCurrency][date_key]["market_data"]["current_price"]["usd"] * row[valueColumn]
+        except KeyError:
+            missing_coingecko_coverage[i] = {"symbol": boughtCurrency, "date": row[date_column]}
+
+        rows[i] = row
+
+    if missing_coingecko_coverage:
+        print("CoinGecko does not provide coverage for the following Symbol + Date combinations:")
+        for index in missing_coingecko_coverage:
+            print(f"\tSymbol: {missing_coingecko_coverage[index]['symbol']}\t\tDate: {missing_coingecko_coverage[index]['date']}\t\t(Excel Row {index + 2})")
+
+        print("Attempting Coinhall fallback API")
+
+        index_to_costs = {}
+        missing_coinhall_coverage = {}
+        print("Reaching out to Coinhall for symbol cost data, this may take a while...")
+        for index in missing_coingecko_coverage:
+            symbol = missing_coingecko_coverage[index]['symbol']
+            date = missing_coingecko_coverage[index]['date']
+            
+            request_url = get_coinhall_request_url(symbol, coinhall_symbols_to_id_configs)
+
+            if request_url:
+                parameterized_url = add_coinhall_request_params(request_url, date, coinhall_symbols_to_id_configs[symbol]["id"])
+                resp = make_coingecko_api_request(parameterized_url, "Error reaching out to CoinGecko, please try again later:", COINHALL_THROTTLE_TIME)
+                data = resp.json()
+                if data and len(data) > 0:
+                    index_to_costs[index] = data[0]
+                else:
+                    missing_coinhall_coverage[index] = {"symbol": symbol, "date": date}
+                time.sleep(COINHALL_THROTTLE_TIME)
+            else:
+                print("Your Coinhall symbol config list does not support symbol", symbol)
+        
+        print("Finished reaching out to Coinhall")
+        for index in index_to_costs:
+            row = rows[index]
+            row["usdValue"] = index_to_costs[index]["high"] * row[valueColumn]
+            rows[index] = row
+
+        if missing_coinhall_coverage:
+            print("CoinHall does not provide coverage for the following Symbol + Date combinations:")
+            for index in missing_coinhall_coverage:
+                print(f"\tSymbol: {missing_coinhall_coverage[index]['symbol']}\t\tDate: {missing_coinhall_coverage[index]['date']}\t\t(Excel Row {index + 2})")
+        else:
+            print("CoinHall provided coverage for all CoinGecko missing symbols")
+
+    return rows
+
+#creates chunks of symbols and dates to send for API processing
+def chunk_symbols_and_dates(symbols_to_dates, len_chunks):
+
+    values = {}
+    length = 0
+    for key in symbols_to_dates:
+        for date in symbols_to_dates[key]:
+            if key in values:
+                values[key].append(date)
+                length+=1
+            else:
+                values[key] = [date]
+                length+=1
+            if length == len_chunks:
+                yield values
+                values = {}
+                length = 0
+
+    #last yield returns last chunk
+    yield values
+
+def get_coingecko_request_url(symbol, coingecko_symbols_to_id_configs):
+    coingecko_request_id = None
+    coingecko_request_url = None
+
+    if symbol in coingecko_symbols_to_id_configs:
+        coingecko_request_id = coingecko_symbols_to_id_configs[symbol]["id"]
+        coingecko_request_url =  COIN_GECKO_API_URL + COIN_GECKO_COINS_ENDPOINT + COIN_GECKO_HISTORICAL_ENDPOINT.replace("<coin-id>", coingecko_request_id)
+
+    return coingecko_request_url
+
+def add_coingecko_request_params(request_url, date):
+    return request_url + f"?date={date}"
+
+def make_coingecko_api_request(request_url, error_string, throttle_time):
+    resp = requests.get(request_url)
+    try:
+        resp.raise_for_status()
+    except requests.HTTPError as err:
+        #if throttled, wait for the throttle to end and retry
+        if resp.status_code == 429:
+            time.sleep(throttle_time)
+            resp = requests.get(request_url)
+
+            #give up after 1 retry
+            try:
+                resp.raise_for_status()
+            except Exception as err:
+                print(error_string, err)
+                sys.exit(1)
+        else:
+            print(error_string, err)
+            sys.exit(1)
+    except Exception as err:
+        print(error_string, err)
+        sys.exit(1)
+    return resp
+
+def get_coinhall_request_url(symbol, coinhall_symbols_to_id_configs):
+    coinhall_request_url = None
+
+    if symbol in coinhall_symbols_to_id_configs:
+        coinhall_request_url =  COINHALL_API_URL + COINHALL_CHART_ENDPOINT
+
+    return coinhall_request_url
+
+def add_coinhall_request_params(request_url, date, symbol_id):
+    from_date = date.replace(hour=0, minute=0, second=0, microsecond=0)
+    to_date = from_date.replace(hour=11, minute=59, second=59)
+
+    from_unix = str(int(time.mktime(from_date.timetuple())))
+    to_unix = str(int(time.mktime(to_date.timetuple())))
+
+    return request_url + COINHALL_HISTORICAL_PARAMS.replace("<from-time>", from_unix).replace("<to-time>", to_unix).replace("<coin-id>", symbol_id)
+
+def make_coinhall_api_request(request_url, error_string, throttle_time):
+    resp = requests.get(request_url)
+    try:
+        resp.raise_for_status()
+    except requests.HTTPError as err:
+        #if throttled, wait for the throttle to end and retry
+        if resp.status_code == 429:
+            time.sleep(throttle_time)
+            resp = requests.get(request_url)
+
+            #give up after 1 retry
+            try:
+                resp.raise_for_status()
+            except Exception as err:
+                print(error_string, err)
+                sys.exit(1)
+        else:
+            print(error_string, err)
+            sys.exit(1)
+    except Exception as err:
+        print(error_string, err)
+        sys.exit(1)
+def output_rows(title, headers, rows, fname):
+    print("Creating new Excel file", fname)
+    wb = Workbook()
+    ws1 = wb.active
+    ws1.title = title
+
+    ws1.append(headers)
+
+    for row in rows:
+        data = []
+        for header in headers:
+            if header in row:
+                data.append(row[header])
+            else:
+                data.append(None)
+        ws1.append(data)
+
+    wb.save(fname)
+
+def main():
+    args = parse_args()
+    validate_args(args)
+
+    #Curated list of CoinGecko Symbols to IDs in a JSON file
+    #These will correspond to the symbols found in your CSV column -> CoinGecko's data structure for corresponding symbol
+    #e.g.
+    # {
+    #     "DAI": {
+    #         "id": "dai",
+    #         "symbol": "dai",
+    #         "name": "Dai"
+    #     },
+    #     "ADA": {
+    #         "id": "cardano",
+    #         "symbol": "ada",
+    #         "name": "Cardano"
+    #     },
+    #     ...
+    # }
+    coingecko_symbols_to_id_configs = load_config_file(args.coingecko_symbol_to_id_file)
+
+    #Curated list of Coinhall Symbols to IDs in a JSON file
+    #Coinhall provides coverage for various Terra Symbols by pairAddress
+    #These will correspond to the symbols found in your CSV column -> Coinhalls's data structure for corresponding symbol
+    #e.g.
+    # {
+    #      "MINE": {
+    #         "id": "terra178jydtjvj4gw8earkgnqc80c3hrmqj4kw2welz",
+    #         "symbol": "mine",
+    #         "name": "Pylon Protocol"
+    #     },
+    #     "PSI": {
+    #         "id": "terra163pkeeuwxzr0yhndf8xd2jprm9hrtk59xf7nqf",
+    #         "symbol": "psi",
+    #         "name": "Nexus Protocol"
+    #     },
+    #     "LOOP": {
+    #         "id": "terra106a00unep7pvwvcck4wylt4fffjhgkf9a0u6eu",
+    #         "symbol": "loop",
+    #         "name": "LOOP Finance"
+    #     },
+    #     ...
+    # }
+    coinhall_symbols_to_id_configs = load_config_file(args.coinhall_symbol_to_id_file)
+
+    title, headers, rows = parse_input_data(args.input_file)
+    key_data_point_indexes = get_key_data_point_indexes(rows, args.date_column, args.year_filter)
+    rows = process_rows(rows, key_data_point_indexes, coingecko_symbols_to_id_configs, coinhall_symbols_to_id_configs, args.date_column, args.symbol_column, args.value_column)
+    headers.append("usdValue")
+    output_rows(title, headers, rows, args.output_file)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except CaughtError as err:
+        print(err)
+    except Exception as err:
+        print("An unanticipated error occured, please contact the developer to assist (provide the following stack trace)")
+        traceback.print_exc()
+        print(err)
